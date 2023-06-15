f1b27cbdc54a8b7e24bd90bba90c12ccf7a5dbdf
stroke00 <136699468+stroke00@users.noreply.github.com>
2023-06-15 15:26:43 +0530

    Add files via upload

---
+++ calcilator.py
@@ -0,0 +1,18 @@
+a=int(input("the_first_number :"))
+b=int(input("the_second_number :"))
+
+s=input("the sign:")
+if s == "+":
+    print(a+b)
+elif s == "-":
+    print(a-b)
+elif s == "/":
+    print(a/b)
+elif s == "*":
+  print(a*b)
+
+else:
+    pass
+   
+
+    
